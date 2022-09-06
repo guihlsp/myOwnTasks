fcfc05b4eb4484b8ed47b11f39c471330d7312a5
root <root@BETELNOTE557.beteltecnologia.net.br>
2022-09-06 02:01:22 -0300

    ajuste css

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,12 @@ export default defineComponent({
 
 
 <style>
-  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"; 
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+   
+  
   
   body{
-  min-height: 100%;
+  height: 100%;
   overflow: auto
   }
 
